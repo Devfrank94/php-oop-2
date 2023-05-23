9bcd807efd772badf0fa465b2510b7b68540ea0c
Devfrank94 <francescomurro94@gmail.com>
2023-05-23 17:02:12 +0200

    add file structure

---
+++ Database/db.php
@@ -0,0 +1,18 @@
+<?php
+
+include __DIR__.'/../Models/Bed.php';
+include __DIR__.'/../Models/Food.php';
+include __DIR__.'/../Models/Genre.php';
+include __DIR__.'/../Models/Product.php';
+include __DIR__.'/../Models/Toy.php';
+
+$products = [
+
+
+
+
+
+
+
+
+];
